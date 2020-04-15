a580b667258f8590ce1e10856fae57ba11a52e09
fanianugroho <hanifanianugroho@gmail.com>
2020-04-15 15:14:20 +0700

    Updates

---
+++ precache-manifest.62a5a631da1dd93820eacdb1e0b076be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b868321359e0511ebd75c62e643a73fd",
+    "url": "/15108-UTS-WEB3A-HaniFaniaNugroho/index.html"
+  },
+  {
+    "revision": "1c0f3739460d978037ca",
+    "url": "/15108-UTS-WEB3A-HaniFaniaNugroho/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "98623773a053d0d91ed5",
+    "url": "/15108-UTS-WEB3A-HaniFaniaNugroho/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c0f3739460d978037ca",
+    "url": "/15108-UTS-WEB3A-HaniFaniaNugroho/static/js/2.3d950de4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/15108-UTS-WEB3A-HaniFaniaNugroho/static/js/2.3d950de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98623773a053d0d91ed5",
+    "url": "/15108-UTS-WEB3A-HaniFaniaNugroho/static/js/main.f1b3736f.chunk.js"
+  },
+  {
+    "revision": "516c693daa5068d7af49",
+    "url": "/15108-UTS-WEB3A-HaniFaniaNugroho/static/js/runtime-main.181547d2.js"
+  }
+]);
